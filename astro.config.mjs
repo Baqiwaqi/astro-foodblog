b0b864c22a389041411ed7c60f2a315d2c88de93
Tim <48436110+Baqiwaqi@users.noreply.github.com>
2023-09-06 14:10:54 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [tailwind()],
-   plugins: ['prettier-plugin-astro'],
-   overrides: [
-      {
-         files: '*.astro',
-         options: {
-            parser: 'astro',
-         },
-      },
-   ],
-   output: 'server'
+  integrations: [tailwind()],
+  plugins: ['prettier-plugin-astro'],
+  overrides: [{
+    files: '*.astro',
+    options: {
+      parser: 'astro'
+    }
+  }],
+  output: 'server',
+  adapter: vercel()
 });
